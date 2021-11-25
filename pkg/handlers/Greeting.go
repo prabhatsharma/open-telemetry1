6a91b42862fbde7e867985f3a6de18374289d8ee
Prabhat Sharma <hi.prabhat@gmail.com>
2021-11-25 06:04:01 -0800

    removed auto from code

--- pkg/handlers/Greeting.go
+++ pkg/handlers/Greeting.go
@@ -9,6 +9,6 @@ import (
 // Greeting function gets all events
 func Greeting(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"otel1": "A sample implementation demonstrating auto instrumenting a go app using gin gonic framework. ",
+		"otel1": "A sample implementation demonstrating instrumenting a go app using gin gonic framework. ",
 	})
 }
